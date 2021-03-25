38de6323bb9ef234c05ed4f18bbc902e65538598
abdallakhattab <khattababdalla@yahoo.com>
2021-03-25 16:12:32 +0200

    just made a prompt

---
+++ app.js
@@ -0,0 +1,5 @@
+var x = prompt('do you love the ney?','yes,no')
+if (x === 'yes'){alert('nice! you got a special taste of music')}else (alert('no problem copy the link to search for another instrument'))
+var y = prompt('whats your name?')
+if (y === 'abdalla'){document.write('<h1>wilcome to your website abdalla<h1>')}else (document.write('<h1>welcome to abdalla website<h1>'))
+console.log(x === y)
