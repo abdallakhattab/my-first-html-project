b48cb6751736065b78a5d8150e6c27365cfeeff5
abdallakhattab <khattababdalla@yahoo.com>
2021-03-29 20:47:45 +0300

    added a function

--- imgimg.js
+++ imgimg.js
@@ -1,5 +1,6 @@
-// var  neynumber = prompt('how much neys you want us to view?');
 
-// for (var i = 1  ; i <= neynumber ; i++)
+var  neynumber = prompt('how much neys you want us to view?');
 
-// {document.write(' <img src = "https://tickets.metabrainz.org/secure/thumbnail/11887/_thumb_11887.png "/>'); }
+for (var i = 1  ; i <= neynumber ; i++)
+
+{document.write(' <img src = "https://tickets.metabrainz.org/secure/thumbnail/11887/_thumb_11887.png "/>'); }
--- password.js
+++ password.js
@@ -1,14 +1,18 @@
-//28/3/2021 class
-//var age = prompt('what is your age')
-//if (age >= 18 && age < 35) { alert('welcome brother')}
-//else if (age < 18 || age < 35) {
-  //  alert('sorry your not allowd');
-//} else { alert('I Love you'); }
-// password input
 
-// var  userpassword = prompt('inter the password ..NOTE: the password is 123');
-// var password = '123';
 
-// while(userpassword !== password){userpassword = prompt('inter the password')}
+function(password){
+var  userpassword = prompt('inter the password ..NOTE: the password is 123');
+var password = '123';
 
-// console.log('<a herf = "https://ts3.arabics.travian.com/login.php"></a>')
+while(userpassword !== password){userpassword = prompt('inter the password')}
+}
+return(password)
+
+function (agenumber){
+  var age = prompt('what is your age')
+  if (age >= 18 && age < 35) { alert('welcome brother')}
+  else if (age < 18 || age < 35) {
+     alert('sorry your not allowd');
+  } else { alert('I Love you'); }
+  }
+  return(agenumber)
